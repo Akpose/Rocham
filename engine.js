f03b4f5a3bfcab61eea276b36ee1bccaf5470389
Akpose <122671706+Akpose@users.noreply.github.com>
2023-06-10 11:16:16 +0100

    ADD neccesary helper functions

--- engine.js
+++ engine.js
@@ -1,47 +1,95 @@
+let player = 0;
+let computer = 0;
+
+function welcome() {
+    alert("Welcome to the old-school game Rock-Paper-Scissors. The rules are simple: Rock beats scissors, scissors beats paper, paper beats rock.");
+}
+
+function game(numOfRounds) {
+    welcome();
+
+    do {
+        let cc = getComputerChoice(array);
+        let pc = getPlayerChoice();
+        let result = compareChoices(cc, pc);
+        updateScore(result, 1);
+
+        if (result !== 0) {
+            numOfRounds--;
+        }
+
+    } while (numOfRounds > 0);
+    if (player > computer) {
+        alert("FINAL SCORE")
+        alert(`Player = ${player}  VS  Computer = ${computer}`);
+        alert("Congratulations! YOU WIN");
+    } 
+    alert(`Player = ${player}  VS  Computer = ${computer}`);
+    alert("SORRY! YOU LOSE");
+
+    clearScoreBoard();
+}
+
+const array = ["Rock", "Paper", "Scissors"];
+
 function getComputerChoice(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
     const item = arr[randomIndex];
-
-    return item;
+    let computerChoice = item.toUpperCase();
+    return computerChoice;
 }
 
-//console.log(getComputerChoice(array));
-
-function playRound(playerSelection, computerSelection) {
-    let player = playerSelection.toUpperCase();
-    let computer = computerSelection.toUpperCase();
-    if (player === computer) {
-        return "A tie! No winner"
-    } else if (player === "ROCK" && computer === "SCISSORS") {
-        return `You Win! ${playerSelection} beats ${computerSelection}`; 
-    } else if (player === "ROCK" && computer === "PAPER") {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    } else if (player === "SCISSORS" && computer ==="ROCK") {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    } else if (player === "SCISSORS" && computer === "PAPER") {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-    } else if (player === "PAPER" && computer === "SCISSORS") {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    } else if (player === "PAPER" && computer === "ROCK") {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-    } else return "iNVALID ENTRY! enter rock, paper or scissors";
+function getPlayerChoice() {
+    let pc = prompt("Choose EITHER 'rock, paper, or scissors.'");
+    let playerChoice = pc.toUpperCase();
+    if (playerChoice === "ROCK" || playerChoice === "SCISSORS" || playerChoice === "PAPER") {
+        alert("Nice one");
+        return playerChoice;
+    } alert("INVALID! ensure your spelling is correct, Choose either rock, paper, or scissors.");
+    getPlayerChoice();
 }
 
-//onst playerSelection = "rock";
-//const array = ['rock', 'paper', 'scissors'];
-//onst computerSelection = getComputerChoice(array);
-//console.log(playRound(playerSelection, computerSelection));
+function compareChoices(computerChoice, playerChoice) {
+    alert(`Your choice = ${playerChoice}  ---VS---  Computer's Choice = ${computerChoice}`);
+    if (playerChoice === computerChoice) {
+        alert("Same choice, It's a tie, Replay!");
+        let result = 0;
+        return result;
+    }
+    if ((playerChoice === "ROCK" && computerChoice === "SCISSORS")
+                                ||
+        (playerChoice === "SCISSORS" && computerChoice === "PAPER")
+                                ||
+        (playerChoice === "PAPER" && computerChoice === "ROCK")) {
+            alert("You win");
+            let result = 1;
+            return result;
+        } alert("You lose");
+        let result = 2;
+        return result;   
+} 
 
-function game() {
-    const playerSelection = window.prompt("Enter any of the following 'Rock, Paper, scissors': ");
-    const array = ['rock', 'paper', 'scissors'];
-    const computerSelection = getComputerChoice(array);
-
-    let i = 0;
-    do {
-        console.log(playRound(playerSelection, computerSelection))
-        i++;
-    } while (i < 5);
-    
+function updateScore(result, points) {
+    if (result == 1) { 
+        player += points;
+        alert(`Player = ${player}  VS  Computer = ${computer}`);
+    }
+    if (result === 2) { 
+        computer += points;
+        alert(`Player = ${player}  VS  Computer = ${computer}`);
+    }
+    if (result === 0) { 
+        player += 0;
+        computer += 0;
+        alert(`Player = ${player}  VS  Computer = ${computer}`);
+    }
 }
-console.log(game());
+
+function clearScoreBoard() {
+    player = 0;
+    computer = 0;
+  }
+
+
+
+game(5);
